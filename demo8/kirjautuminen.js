db111c70648ae390f63a0081259cbc7fcddf5f0b
olena-petrova <152591627+olena-petrova@users.noreply.github.com>
2024-05-19 21:02:38 +0300

    demo8

--- demo8/kirjautuminen.js
+++ demo8/kirjautuminen.js
@@ -1,14 +1,28 @@
 document.addEventListener("DOMContentLoaded", onkoKirjautunut);
 
 function onkoKirjautunut() {
-    let kirjautunut = 'kylla';
-    if (localStorage.getItem("kirjautunut") === 'kylla') {
-        document.getElementById('tervetulo_teksti').textContent += localStorage.getItem("nimi");
+    if (localStorage.getItem("kirjautunut") === "kylla") {
+        document.getElementById("tervetulo_teksti").textContent += localStorage.getItem("nimi");
         document.getElementById("kirjautumis_lomake").style.display = "none";
+        document.getElementById("kirjaudu_ulos_nappi").style.display = "block";
+    } else {
+        document.getElementById("kirjautumis_lomake").style.display = "block";
+        document.getElementById("kirjaudu_ulos_nappi").style.display = "none";
     }
 }
 
 function kirjaudu() {
-    localStorage.setItem("nimi", document.getElementById("nimi").value);
-    localStorage.setItem("kirjautunut", "kylla");
+    const nimi = document.getElementById("nimi").value.trim();
+    if (nimi) {
+        localStorage.setItem("nimi", nimi);
+        localStorage.setItem("kirjautunut", "kylla");
+    } else {
+        alert("Syötä Nimesi");
+    }
+}
+
+function kirjauduUlos() {
+    localStorage.clear();
+    document.getElementById("tervetulo_teksti").textContent = "Tervetuloa ";
+    onkoKirjautunut();
 }
