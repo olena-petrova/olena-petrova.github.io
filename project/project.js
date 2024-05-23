a438504e9a153dba3171f76369efdc5bcda61050
olena-petrova <152591627+olena-petrova@users.noreply.github.com>
2024-05-23 09:58:51 +0300

    komentoja2

--- project/project.js
+++ project/project.js
@@ -1,12 +1,15 @@
+//prosessoinmuuttujien luominen
 const panels = document.querySelectorAll('.panel');
 
+//luo tapahtumakäsittelijä
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
-        removeActiveClasses();
-        panel.classList.add('active');
+        removeActiveClasses();                 //funktion kutsuminen
+        panel.classList.add('active');         //painetun painikkeen aktiivinen tila
     })
 })
 
+//funktio, joka poistu aktiivinen tila, jos painat joku muu kuva
 function removeActiveClasses(){
     panels.forEach(panel => {
         panel.classList.remove('active');
